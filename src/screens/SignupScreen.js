9f06602f8708f2d46cb82bfe5b125e98b3428abc
Guilherme Silva <silva.gsg@gmail.com>
2023-12-03 21:47:24 +0000

    feat: Create app screens boilerplate

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Signup Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: { fontSize: 48 },
+});
+
+export default SignupScreen;
